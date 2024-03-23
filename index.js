e41cd89984192f4b9a8c8ffc2a75bf93ca0b929f
siyajari samuel <siyajari.samuel@student.moringaschool.com>
2024-03-24 00:44:59 +0300

    Created Array find method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+function superbowlWin(record) {
+    // Use the find method to search for a win (result === 'W') in the record array
+    const winningYear = record.find(game => game.result === 'W');
+
+    // If a win is found, return the year of the win
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        // If no win is found, return undefined
+        return undefined;
+    }
+}
+
+
